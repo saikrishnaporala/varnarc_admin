91838b95ac123daee9bf09f004835563f3d74419
saiporala <saikrishnaporala@gmail.com>
2025-09-13 15:49:08 +0530

    command js build update

--- public/build/js/pages/command-exec.init.js
+++ public/build/js/pages/command-exec.init.js
@@ -24,7 +24,7 @@ $(document).ready(function () {
         });
     });
 
-    // Load contacts into table
+    // Load commands into table
     function loadCommandLogs() {
         $.get('/api/command-logs', function(data) {
             let tbody = $("#customerTable tbody");
@@ -36,44 +36,12 @@ $(document).ready(function () {
             }
             $(".noresult").hide();
 
-            data.forEach(contact => {
+            data.forEach(commands => {
                 tbody.append(`
                     <tr>
-                        <td>
-                            <div class="form-check">
-                                <input class="form-check-input" type="checkbox" name="chk_child" value="${contact.id}">
-                            </div>
-                        </td>
-                        <td class="name">
-                            <div class="d-flex align-items-center">
-                                <div class="flex-shrink-0">
-                                    <img src="${contact.image_url ?? '/build/images/users/user-dummy-img.jpg'}"
-                                        alt="" class="avatar-xs rounded-circle">
-                                </div>
-                                <div class="flex-grow-1 ms-2">${contact.name}</div>
-                            </div>
-                        </td>
-                        <td class="company_name">${contact.company_name ?? ''}</td>
-                        <td class="email_id">${contact.email}</td>
-                        <td class="phone">${contact.phone ?? ''}</td>
-                        <td class="lead_score">${contact.lead_score ?? ''}</td>
-                        <td class="tags">${contact.tags ?? ''}</td>
-                        <td class="date">${contact.updated_at ?? ''}</td>
-                        <td>
-                            <div class="dropdown">
-                                <button class="btn btn-soft-secondary btn-sm dropdown" data-bs-toggle="dropdown">
-                                    <i class="ri-more-fill align-middle"></i>
-                                </button>
-                                <ul class="dropdown-menu dropdown-menu-end">
-                                    <li><a class="dropdown-item view-item-btn" href="javascript:void(0);" data-id="${contact.id}">
-                                        <i class="ri-eye-fill align-bottom me-2 text-muted"></i> View</a></li>
-                                    <li><a class="dropdown-item edit-item-btn" href="javascript:void(0);" data-id="${contact.id}">
-                                        <i class="ri-pencil-fill align-bottom me-2 text-muted"></i> Edit</a></li>
-                                    <li><a class="dropdown-item remove-item-btn" href="javascript:void(0);" data-id="${contact.id}">
-                                        <i class="ri-delete-bin-fill align-bottom me-2 text-muted"></i> Delete</a></li>
-                                </ul>
-                            </div>
-                        </td>
+                        <td class="company_name">${commands.command ?? ''}</td>
+                        <td class="email_id">${commands.user_id}</td>
+                        <td class="phone">${commands.created_at}</td>
                     </tr>
                 `);
             });
