5ddba9a5bf0a9feda130c21e407f996a3cc7c201
saiporala <saikrishnaporala@gmail.com>
2025-09-14 23:42:27 +0530

    datatables

--- resources/views/data-dump.blade.php
+++ resources/views/data-dump.blade.php
@@ -505,7 +505,7 @@
                             </tbody>
                         </table>
                         <div class="table-responsive table-card mb-3">
-                            <table class="table table-bordered dt-responsive nowrap table-striped align-middle" id="driveFilesTable">
+                            <table class="table table-bordered dt-responsive nowrap table-striped align-middle" id="driveFilesTable" style="width:100%">
                                 <thead class="table-light">
                                     <tr>
                                         <th scope="col" style="width: 50px;">
@@ -540,103 +540,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="modal fade" id="showModal" tabindex="-1"
-                        aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog modal-dialog-centered">
-                            <div class="modal-content border-0">
-                                <div class="modal-header bg-info-subtle p-3">
-                                    <h5 class="modal-title" id="exampleModalLabel"></h5>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                        aria-label="Close" id="close-modal"></button>
-                                </div>
-                                <form class="tablelist-form" autocomplete="off">
-                                    <div class="modal-body">
-                                        <input type="hidden" id="id-field" />
-                                        <div class="row g-3">
-                                            <div class="col-lg-12">
-                                                <div class="text-center">
-                                                    <div class="position-relative d-inline-block">
-                                                        <div class="position-absolute  bottom-0 end-0">
-                                                            <label for="customer-image-input" class="mb-0"  data-bs-toggle="tooltip" data-bs-placement="right" title="Select Image">
-                                                                <div class="avatar-xs cursor-pointer">
-                                                                    <div class="avatar-title bg-light border rounded-circle text-muted">
-                                                                        <i class="ri-image-fill"></i>
-                                                                    </div>
-                                                                </div>
-                                                            </label>
-                                                            <input class="form-control d-none" value="" id="customer-image-input" type="file"
-                                                                accept="image/png, image/gif, image/jpeg">
-                                                        </div>
-                                                        <div class="avatar-lg p-1">
-                                                            <div class="avatar-title bg-light rounded-circle">
-                                                                <img src="{{ URL::asset('build/images/users/user-dummy-img.jpg') }}"
-                                                        alt="" id="customer-img" class="avatar-md rounded-circle object-fit-cover" >
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div>
-                                                    <label for="name-field" class="form-label">Name</label>
-                                                    <input type="text" id="customername-field" class="form-control" placeholder="Enter name" required />
-                                                </div>
-                                            </div>
-                                            <div class="col-lg-12">
-                                                <div>
-                                                    <label for="company_name-field" class="form-label">Company Name</label>
-                                                    <input type="text" id="company_name-field" class="form-control" placeholder="Enter company name" required />
-                                                </div>
-                                            </div>
-                                            <div class="col-lg-12">
-                                                <div>
-                                                    <label for="designation-field" class="form-label">Designation</label>
-                                                    <input type="text" id="designation-field" class="form-control" placeholder="Enter Designation" required />
-                                                </div>
-                                            </div>
-                                            <div class="col-lg-12">
-                                                <div>
-                                                    <label for="email_id-field" class="form-label">Email ID</label>
-                                                    <input type="text" id="email_id-field" class="form-control" placeholder="Enter email" required />
-                                                </div>
-                                            </div>
-                                            <div class="col-lg-6">
-                                                <div>
-                                                    <label for="phone-field" class="form-label">Phone</label>
-                                                    <input type="text" id="phone-field" class="form-control" placeholder="Enter phone no" required />
-                                                </div>
-                                            </div>
-                                            <div class="col-lg-6">
-                                                <div>
-                                                    <label for="lead_score-field" class="form-label">Lead Score</label>
-                                                    <input type="text" id="lead_score-field" class="form-control" placeholder="Enter value" required />
-                                                </div>
-                                            </div>
-                                            <div class="col-lg-12">
-                                                <div>
-                                                    <label for="taginput-choices" class="form-label font-size-13 text-muted">Tags</label>
-                                                    <select class="form-control" name="taginput-choices"
-                                                        id="taginput-choices"  multiple>
-                                                        <option value="Lead">Lead</option>
-                                                        <option value="Partner">Partner</option>
-                                                        <option value="Exiting">Exiting</option>
-                                                        <option value="Long-term">Long-term</option>
-                                                    </select>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="modal-footer">
-                                        <div class="hstack gap-2 justify-content-end">
-                                            <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
-                                            <button type="submit" class="btn btn-success" id="add-btn">Add Contact</button>
-                                            {{-- <button type="button" class="btn btn-success" id="edit-btn">Update</button> --}}
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                    <!--end add modal-->
-
+                    
                     <div class="modal fade zoomIn" id="deleteRecordModal" tabindex="-1"
                         aria-hidden="true">
                         <div class="modal-dialog modal-dialog-centered">
@@ -667,8 +571,6 @@
                             </div>
                         </div>
                     </div>
-                    <!--end delete modal -->
-
                 </div>
             </div>
             <!--end card-->
@@ -737,82 +639,8 @@
 
                         <button type="submit" class="btn btn-primary w-100">Import</button>
                     </form>
-
                 </div>
             </div>
-            <div class="card" id="contact-view-detail">
-                <div class="card-body text-center">
-                    <div class="position-relative d-inline-block">
-                        <img src="{{ URL::asset('build/images/users/avatar-10.jpg') }}" alt=""
-                            class="avatar-lg rounded-circle img-thumbnail">
-                        <span class="contact-active position-absolute rounded-circle bg-success"><span
-                                class="visually-hidden"></span>
-                    </div>
-                    <h5 class="mt-4 mb-1">Tonya Noble</h5>
-                    <p class="text-muted">Nesta Technologies</p>
-
-                    <ul class="list-inline mb-0">
-                        <li class="list-inline-item avatar-xs">
-                            <a href="javascript:void(0);"
-                                class="avatar-title bg-success-subtle text-success fs-15 rounded">
-                                <i class="ri-phone-line"></i>
-                            </a>
-                        </li>
-                        <li class="list-inline-item avatar-xs">
-                            <a href="javascript:void(0);"
-                                class="avatar-title bg-danger-subtle text-danger fs-15 rounded">
-                                <i class="ri-mail-line"></i>
-                            </a>
-                        </li>
-                        <li class="list-inline-item avatar-xs">
-                            <a href="javascript:void(0);"
-                                class="avatar-title bg-warning-subtle text-warning fs-15 rounded">
-                                <i class="ri-question-answer-line"></i>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="card-body">
-                    <h6 class="text-muted text-uppercase fw-semibold mb-3">Personal Information</h6>
-                    <p class="text-muted mb-4">Hello, I'm Tonya Noble, The most effective objective is
-                        one that is tailored to the job you are applying for. It states what kind of
-                        career you are seeking, and what skills and experiences.</p>
-                    <div class="table-responsive table-card">
-                        <table class="table table-borderless mb-0">
-                            <tbody>
-                                <tr>
-                                    <td class="fw-medium" scope="row">Designation</td>
-                                    <td>Lead Designer / Developer</td>
-                                </tr>
-                                <tr>
-                                    <td class="fw-medium" scope="row">Email ID</td>
-                                    <td>tonyanoble@velzon.com</td>
-                                </tr>
-                                <tr>
-                                    <td class="fw-medium" scope="row">Phone No</td>
-                                    <td>414-453-5725</td>
-                                </tr>
-                                <tr>
-                                    <td class="fw-medium" scope="row">Lead Score</td>
-                                    <td>154</td>
-                                </tr>
-                                <tr>
-                                    <td class="fw-medium" scope="row">Tags</td>
-                                    <td>
-                                        <span class="badge bg-primary-subtle text-primary">Lead</span>
-                                        <span class="badge bg-primary-subtle text-primary">Partner</span>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td class="fw-medium" scope="row">Last Contacted</td>
-                                    <td>15 Dec, 2021 <small class="text-muted">08:58AM</small></td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-            <!--end card-->
         </div>
         <!--end col-->
     </div>
