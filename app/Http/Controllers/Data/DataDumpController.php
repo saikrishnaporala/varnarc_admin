52e9ff1de1e6f50eaf3282de9b0b2572fc918fe7
saiporala <saikrishnaporala@gmail.com>
2025-09-24 21:38:19 +0530

    increase memory_limit

--- app/Http/Controllers/Data/DataDumpController.php
+++ app/Http/Controllers/Data/DataDumpController.php
@@ -35,7 +35,7 @@ class DataDumpController extends Controller
 
     public function importFromGoogleDrive(Request $request)
     {
-        ini_set('memory_limit', '1024M');
+        ini_set('memory_limit', '2048M');
         ini_set('max_execution_time', 0); // no time limit
         set_time_limit(0);
 
